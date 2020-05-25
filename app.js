ccb58b596b72181eda68c71ab989c80fa3ea52c8
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 18:55:18 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const express = require('express');
-const mysql = require('mysql');
 const app = express();
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
-
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'progate',
-  password: 'password',
-  database: 'list_app'
-});
 
 app.get('/', (req, res) => {
   res.render('top.ejs');
