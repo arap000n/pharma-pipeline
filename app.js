427a36d70149dc3c90bcf01d917cec7203f03f4b
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 21:47:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('https://github.com/arap000n/pharma-pipeline.git/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.render('https://github.com/arap000n/pharma-pipeline.git/public/top.ejs');
+  res.render('top.ejs');
 });
 
 express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
