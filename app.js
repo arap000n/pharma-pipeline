1fe52bab2f773d8257ba50b01176da3e915400b7
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 22:02:59 +0900

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5000
-
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
-
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
