9bb6c9fb643300a52e4292038fe0b82fec2f4ab6
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 21:30:06 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
