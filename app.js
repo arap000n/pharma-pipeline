82dcd6657d6c7643061e6fade30990362aa9c9c6
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 22:04:36 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
