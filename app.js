684fb3abbd8d79fdba3127ba77910c2bfb47acc6
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-25 22:02:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,4 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 
-app.get('/', (req, res) => {
-  res.render('top.ejs');
-});
-
 express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
