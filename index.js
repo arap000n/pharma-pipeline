df9780bd1201a6fe19b74d0177bfb063b3d11858
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-26 21:22:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+const connection = mysql.createConnection({
+  host: "us-cdbr-east-05.cleardb.net",
+  user: "b9b886717a0254",
+  password: "84f508e2",
+  database: "heroku_0edb62883326224"
+});
