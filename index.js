e8106211ce1003751997d8fe866ef6b736b167bc
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-26 23:48:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,6 @@ const path = require('path')
 const mysql = require('mysql');
 const PORT = process.env.PORT || 5000
 
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-
 const connection = mysql.createConnection({
   host: "us-cdbr-east-05.cleardb.net",
   user: "b9b886717a0254",
@@ -16,12 +10,15 @@ const connection = mysql.createConnection({
   database: "heroku_0edb62883326224"
 });
 
-express().get('/', (req, res) => {
-  connection.query(
-    'SELECT * FROM drug',
-    (error, results) => {
-      res.render('pages/index',{drugs: results});
-    })
-})
-
-express().listen(PORT, () => console.log(`Listening on ${ PORT }`))
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => {
+    connection.query(
+      'SELECT * FROM drug',
+      (error, results) => {
+        res.render('pages/index',{drugs: results});
+      })
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
