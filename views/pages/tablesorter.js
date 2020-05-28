debfc6c05ee7847b33b1f2d282454bad679dd9cf
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-28 22:30:03 +0900

    Update tablesorter.js

--- views/pages/tablesorter.js
+++ views/pages/tablesorter.js
@@ -1,13 +1,3 @@
 $(document).ready(function() { 
-  $("#myTable").tablesorter({
-    headers: {
-      1: { sorter:"text"},
-      2: { sorter:"text"},
-      3: { sorter:"text"},
-      4: { sorter:"text"},
-      5: { sorter:"text"},
-      6: { sorter:"text"},
-      7: { sorter:"text"}
-    }
-  });
+  $("#myTable").tablesorter();
 });
