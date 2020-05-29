20965db6ad4f343987da0c82a8c0c0359eba1106
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-29 22:46:36 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ express()
   })
   .get('/search', (req, res) => {
     connection.query(
-      'SELECT  company.company_name, drug.dev_code, drug.generic_name, drug.drug_name, pipeline.indication, drug.mechanism, pipeline.phase FROM pipeline LEFT JOIN company ON company.company_id=pipeline.company_id LEFT JOIN drug ON drug.drug_id=pipeline.drug_id WEHRE company.company_name LIKE CONCAT('%',IFNULL(?, ''),'%') AND pipeline.indication LIKE CONCAT('%',IFNULL(?, ''),'%')',
+      'SELECT  company.company_name, drug.dev_code, drug.generic_name, drug.drug_name, pipeline.indication, drug.mechanism, pipeline.phase FROM pipeline LEFT JOIN company ON company.company_id=pipeline.company_id LEFT JOIN drug ON drug.drug_id=pipeline.drug_id WEHRE company.company_name LIKE CONCAT('%',IFNULL(?, ''),'%') AND pipeline.indication LIKE CONCAT('%',IFNULL(?, ''),'%') ',
       [req.body.company_name,req.body.indication],
       (error, results) => {
         res.render('pages/index',{pipelines: results});
