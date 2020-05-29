f5e5b53071cfa76e120c7ec061f3ee379cbe62f3
arap000n <36828885+arap000n@users.noreply.github.com>
2020-05-29 22:59:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ express()
   })
   .get('/search', (req, res) => {
     connection.query(
-      'SELECT  company.company_name, drug.dev_code, drug.generic_name, drug.drug_name, pipeline.indication, drug.mechanism, pipeline.phase FROM pipeline LEFT JOIN company ON company.company_id=pipeline.company_id LEFT JOIN drug ON drug.drug_id=pipeline.drug_id',
+      'SELECT  company.company_name, drug.dev_code, drug.generic_name, drug.drug_name, pipeline.indication, drug.mechanism, pipeline.phase FROM pipeline LEFT JOIN company ON company.company_id=pipeline.company_id LEFT JOIN drug ON drug.drug_id=pipeline.drug_id WHERE company.company_name LIKE ? OR pipeline.indication LIKE ?',
+      [req.body.company_name,req.body.indication],
       (error, results) => {
         res.render('pages/index',{pipelines: results});
       })
